d3f493355eb8cffe1ad9ef8d84568f4e9c4d13cd
Matt Camp <matt@aicampground.com>
2024-11-21 21:03:12 -0600

    test this out

--- flake.nix
+++ flake.nix
@@ -1,6 +1,8 @@
 {
   inputs = {
-    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.2405.*";
+
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    # nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.2405.*";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -13,77 +15,83 @@
 
   outputs = { self, ... }@inputs:
     let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forAllSystems = f: inputs.nixpkgs.lib.genAttrs supportedSystems (system: f rec {
-        pkgs = import inputs.nixpkgs {
-          inherit system;
-          overlays = [
-            inputs.rust-overlay.overlays.default
-          ];
-        };
+      supportedSystems =
+        [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = f:
+        inputs.nixpkgs.lib.genAttrs supportedSystems (system:
+          f rec {
+            pkgs = import inputs.nixpkgs {
+              inherit system;
+              overlays = [ inputs.rust-overlay.overlays.default ];
+            };
 
-        cranePkgs = pkgs.callPackage ./crane.nix {
-          inherit (inputs) crane;
-          inherit supportedSystems;
-          darwinFrameworks = with pkgs.darwin.apple_sdk.frameworks; [ Security SystemConfiguration ];
-        };
-      });
-    in
-    {
+            cranePkgs = pkgs.callPackage ./crane.nix {
+              inherit (inputs) crane;
+              inherit supportedSystems;
+              darwinFrameworks = with pkgs.darwin.apple_sdk.frameworks; [
+                Security
+                SystemConfiguration
+              ];
+            };
+          });
+    in {
       packages = forAllSystems ({ cranePkgs, ... }: rec {
         inherit (cranePkgs) flake-checker;
         default = flake-checker;
       });
 
       devShells = forAllSystems ({ pkgs, cranePkgs }: {
-        default =
-          let
-            check-nixpkgs-fmt = pkgs.writeShellApplication {
-              name = "check-nixpkgs-fmt";
-              runtimeInputs = with pkgs; [ git nixpkgs-fmt ];
-              text = ''
-                git ls-files '*.nix' | xargs nixpkgs-fmt --check
-              '';
-            };
-            check-rustfmt = pkgs.writeShellApplication {
-              name = "check-rustfmt";
-              runtimeInputs = [ cranePkgs.rustNightly ];
-              text = "cargo fmt --check";
-            };
-            get-allowed-refs = pkgs.writeShellApplication {
-              name = "get-allowed-refs";
-              runtimeInputs = [ cranePkgs.rustNightly ];
-              text = "cargo run --features allowed-refs -- --get-allowed-refs";
-            };
-          in
-          pkgs.mkShell {
-            packages = (with pkgs; [
-              bashInteractive
-
-              # Rust
-              cranePkgs.rustNightly
-              cargo-bloat
-              cargo-edit
-              cargo-machete
-              cargo-watch
-              rust-analyzer
-
-              # Nix
-              nixpkgs-fmt
-
-              # CI checks
-              check-nixpkgs-fmt
-              check-rustfmt
-
-              # Scripts
-              get-allowed-refs
-            ]) ++ pkgs.lib.optionals pkgs.stdenv.isDarwin (with pkgs.darwin.apple_sdk.frameworks; [ Security SystemConfiguration ]);
-
-            env = {
-              # Required by rust-analyzer
-              RUST_SRC_PATH = "${cranePkgs.rustNightly}/lib/rustlib/src/rust/library";
-            };
+        default = let
+          check-nixpkgs-fmt = pkgs.writeShellApplication {
+            name = "check-nixpkgs-fmt";
+            runtimeInputs = with pkgs; [ git nixpkgs-fmt ];
+            text = ''
+              git ls-files '*.nix' | xargs nixpkgs-fmt --check
+            '';
           };
+          check-rustfmt = pkgs.writeShellApplication {
+            name = "check-rustfmt";
+            runtimeInputs = [ cranePkgs.rustNightly ];
+            text = "cargo fmt --check";
+          };
+          get-allowed-refs = pkgs.writeShellApplication {
+            name = "get-allowed-refs";
+            runtimeInputs = [ cranePkgs.rustNightly ];
+            text = "cargo run --features allowed-refs -- --get-allowed-refs";
+          };
+        in pkgs.mkShell {
+          packages = (with pkgs; [
+            bashInteractive
+
+            # Rust
+            cranePkgs.rustNightly
+            cargo-bloat
+            cargo-edit
+            cargo-machete
+            cargo-watch
+            rust-analyzer
+
+            # Nix
+            nixpkgs-fmt
+
+            # CI checks
+            check-nixpkgs-fmt
+            check-rustfmt
+
+            # Scripts
+            get-allowed-refs
+          ]) ++ pkgs.lib.optionals pkgs.stdenv.isDarwin
+            (with pkgs.darwin.apple_sdk.frameworks; [
+              Security
+              SystemConfiguration
+            ]);
+
+          env = {
+            # Required by rust-analyzer
+            RUST_SRC_PATH =
+              "${cranePkgs.rustNightly}/lib/rustlib/src/rust/library";
+          };
+        };
       });
     };
 }
